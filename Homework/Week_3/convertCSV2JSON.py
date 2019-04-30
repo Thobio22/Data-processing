5ea1011c5067578398e8fd102a6e1ad090555b0e
Thobio22 <thomas.verouden@hotmail.com>
2019-04-30 16:52:40 +0200

    hw week_3 nu nog tekenen

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -2,41 +2,49 @@
 # Name: Thomas Verouden
 # Student number: 10779272
 """
-Dit script maakt een linechart van de gekozen dataset
-Vraag: Is er een verschil in hoogste windstoten boven schiphol in de afgelopen 10 jaar?
+This script converts a chosen data file (.txt or .csv) to JSON
 """
 
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from sys import argv
 
 
-# def read2pandas(file_name):
-#     """
-#     This converteerd de data naar pandas dataset
-#     verwijderd de # header in de KNMI file
-#     """
-#     exclude = '#'
-#     dataframe = pd.read_csv(file_name)
-#     dataframe = dataframe[~dataframe.Column1.str.contains(exclude)]
-#
-#     return dataframe
+def convert_to_json(dataframe):
+    """
+    This converts the input data to json format
+    """
+    dataframe = dataframe.set_index('YYYYMMDD').to_json('schiphol_windstoten.json', orient = 'index')
 
-# def read2pandas(file_name):
-#     """
-#     slaat rijen over met '#'
-#     """
-#
-#     skip_rows = 0
-#     with open(file_name, 'r') as f:
-#         for line in f:
-#             if line.contains('#'):
-#                 skip_rows += 1
-#             else:
-#                 break
-#
-#     dataframe = pd.read_csv(file_name, skiprows=skip_rows)
-#     return dataframe
+
+def getDataframe(file_name):
+    """
+    This pulls the data set in the folder and converts it to a dataframe using read2pandas
+    """
+    # maak pandas dataframe van KNMI zonder comments
+    if '.csv' in file_name:
+        dataframe = pd.read_csv(file_name, delimiter = ';', comment='#')
+        return dataframe
+    elif '.txt' in file_name:
+        dataframe = pd.read_csv(file_name, delimiter = ',', comment='#')
+        return dataframe
+    else:
+        quit('Usage: use files of .csv or .txt format')
+
+
+def clean(dataframe):
+    """
+    This cleans the dataframe for visualisation
+    Returns a cleansed subdata
+    """
+    # replace 'unknown' in Pop. density with np.nan
+    dataframe = dataframe.replace('unknown', np.nan)
+
+    # remove spaces from column names and content
+    dataframe.columns = dataframe.columns.str.strip()
+
+    return dataframe
+
 
 if __name__ == "__main__":
 
@@ -47,16 +55,18 @@ if __name__ == "__main__":
     file_name = argv[1]
     print(file_name)
 
+    # check if file_name == actual file in folder
     if file_name == False:
         quit('Usage: python file_name')
 
-    # maak pandas dataframe van KNMI zonder comments
-    # dataframe = read2pandas(file_name)
-    if '.csv' in file_name:
-        dataframe = pd.read_csv(file_name, comment='#')
-    elif '.txt' in file_name:
-        dataframe = pd.read_csv(file_name, delimiter = ',', comment='#')
-    else:
-        quit('Usage: use files of .csv or .txt format')
+    # make dataframe from data file
+    dataframe = getDataframe(file_name)
 
-    print(dataframe)
+    # clean the dataframe
+    dataframe = clean(dataframe)
+
+    # take subset from needed columns only: ONLY USE THIS IF NECESSARY!!
+    dataframe = dataframe[['YYYYMMDD', 'FXX']]
+
+    # convert the dataframe to JSON
+    convert_to_json(dataframe)
