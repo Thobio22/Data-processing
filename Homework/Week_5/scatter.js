a515aaaa042570400f0e69266ad1b74c6fe4c25e
Thobio22 <thomas.verouden@hotmail.com>
2019-05-16 00:03:32 +0200

    stapje dichterbij voor legenda

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -13,7 +13,7 @@
 // global variables
 var h = 600;
 
-var w = 600;
+var w = 1200;
 
 var barPad = 5;
 
@@ -27,8 +27,14 @@ var chartPad = {
   top: 100,
   bottom: 100,
   left: 100,
-  right: 100
+  right: 700
 };
+
+// create color array for chart + legend
+var colorArray = ['#bd0026', '#fc4e2a', '#feb24c', '#ffeda0'];
+
+// create value array of gdp portions for chart colors + legend
+var gdpPortions = [gdp_max * (1/4), gdp_max * (2/4), gdp_max * (3/4), gdp_max]
 
 
 window.onload = function() {
@@ -39,6 +45,9 @@ window.onload = function() {
 
   var requests = [d3.json(teensInViolentArea), d3.json(teenPregnancies), d3.json(GDP)];
 
+  // add title, name, studentID and short description
+  addText();
+
 
   Promise.all(requests).then(function(response) {
 
@@ -47,7 +56,6 @@ window.onload = function() {
       var teenPreg = transformResponse(response[1]);
 
       var gdp = transformResponseGDP(response[2]);
-
 
       // console.log(vioArea);
       // console.log(teenPreg);
@@ -137,6 +145,28 @@ window.onload = function() {
 
 };
 
+
+function addText() {
+  // add title, name, studentID and short description
+  d3.select("body")
+    .append("h1")
+    .text("The correlation between percentage teen pregnancy and the percentage of teens in violent areas")
+    .style("font-weight", "bold");
+
+  d3.select("body")
+    .append("h3")
+    .text("Thomas Verouden");
+
+  d3.select("body")
+    .append("h3")
+    .text("StudentID: 10779272");
+
+  d3.select("body")
+    .append("p")
+    .text("This scatterplot made with D3 shows the percentage of teens in violent areas \
+    against the precentage of teen pregnancies in that country. The color is an \
+    indicator for the GDP of that country.");
+};
 
 function addViolent(full_dataset, vioArea) {
   for (i in vioArea) {
@@ -228,7 +258,9 @@ function getXscale(data_limit, chartPad) {
   var xScale = d3.scaleLinear()
                  .domain([0, data_limit])
                  .range([chartPad.left, w - chartPad.right]);
+
   return xScale;
+
 };
 
 
@@ -236,7 +268,9 @@ function getYscale(data_limit, chartPad) {
   var yScale = d3.scaleLinear()
                  .domain([0, data_limit])
                  .range([h - chartPad.bottom, chartPad.top]);
+
   return yScale;
+
 };
 
 
@@ -266,10 +300,13 @@ function drawScatter(data, xScale, yScale) {
      .attr("transform", "translate(" + chartPad.top + ",0)")
      .call(yAxis)
 
+
+
+
   // create color variable that changes with gdp
   var gdp_color = d3.scaleThreshold()
-                    .domain([gdp_max * (1/4), gdp_max * (2/4), gdp_max * (3/4), gdp_max])
-                    .range(['#bd0026', '#fc4e2a', '#feb24c', '#ffeda0']);
+                    .domain(gdpPortions)
+                    .range(colorArray);
 
   // define interaction tooltip
   var tip = d3.tip()
@@ -282,22 +319,11 @@ function drawScatter(data, xScale, yScale) {
                        + "<br>Teens in violent areas: " + d["Vio"] + "%" + "</br>"
                        + "<br>Teen Pregnancies: " + d["Preg"] + "%" + "</br>";
 
-              });
+              })
+              .attr("stroke", "black");
 
   // make interactive animation work
   svg.call(tip);
-
-  console.log(data)
-
-  // Create a dropdown
-  var drop_years = d3.select("#yearDropDown")
-                     .append("select")
-                   	 .selectAll("option")
-                     .data(data)
-                     .enter()
-                     .append("option")
-                     .attr("value", data.key)
-                     .text(data.key)
 
 
   // draw the circles
@@ -320,4 +346,68 @@ function drawScatter(data, xScale, yScale) {
      })
      .on('mouseover', tip.show) // for animation
      .on('mouseout', tip.hide);
+
+     // create chart Title
+     svg.append("text")
+        .attr("transform", "translate(100,0)")
+        .attr("x", 50)
+        .attr("y", 50)
+        .attr("font-size", "24px")
+        .text("Correlation between teen pregnancy and the % teens in violent areas");
+
+     // create x-axis label
+     svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("x", w - chartPad.right)
+        .attr("y", h - 50)
+        .text("% Teens in violent areas");
+
+     // create y-axis label
+     svg.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("y", 30)
+        .attr("x", -150)
+        .attr("dy", "1em")
+        .attr("transform", "rotate(-90)")
+        .text("Teen pregnancy (%)");
+
+     // create legend
+     var legend3 = svg.selectAll('.legend3')
+                      .data(colorArray)
+                      .enter()
+                      .append('g')
+                      .attr("class", "legends3")
+                      .attr("transform", function (d, i) {
+                          return "translate(0," + i * 20 + ")";
+
+                      })
+                      .append('rect')
+                      .attr("x", 0)
+                      .attr("y", 0)
+                      .attr("width", 10)
+                      .attr("height", 10)
+                      .style("fill", function (d, i) {
+                          return colorArray[i];
+
+                      })
+                      .data(gdpPortions)
+                      .enter()
+                      .append('text')
+                      .attr("x", 20)
+                      .attr("y", 10)
+                      //.attr("dy", ".35em")
+                      .text(function (d, i) {
+                          console.log(d)
+                          console.log(i)
+                          return d
+                      })
+                      .attr("class", "textselected")
+                      .style("text-anchor", "start")
+                      .style("font-size", 15);
+
+
+
+
 };
