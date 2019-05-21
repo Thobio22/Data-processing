6f20ca9aa693f8cc5a2f16e7acad2d625892411f
Thobio22 <thomas.verouden@hotmail.com>
2019-05-22 00:09:26 +0200

    begin week 6

---
+++ Homework/Week_6/linked.py
@@ -0,0 +1,10 @@
+#  !/usr/bin/env python
+#      Name: Thomas Verouden
+#      Student number: 10779272
+#
+#      This file visualises the amount of teens in violent areas against the
+#      amount of teen pregnancies in a d3 scatterplot, with the Country GDP indicated by color.
+#
+#      source: http://happyplanetindex.org/resources ==> data
+#      python -m http.server 8888 &
+#      http://localhost:8888/scatter.html
