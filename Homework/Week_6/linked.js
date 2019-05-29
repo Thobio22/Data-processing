e89d68e557dd4b592b6de3fb1dd73a6e5efba863
Thobio22 <thomas.verouden@hotmail.com>
2019-05-29 22:48:37 +0200

    map linked hoe

--- Homework/Week_6/linked.js
+++ Homework/Week_6/linked.js
@@ -22,8 +22,8 @@ window.onload = function() {
   d3v5.json("happiness.json").then(function(dataset){
 
     var chartPad = {
-      h: 1200,
-      w: 400,
+      h: 600,
+      w: 500,
       top: 100,
       bottom: 100,
       left: 100,
@@ -35,12 +35,21 @@ window.onload = function() {
 
 
     // clicked_dataset will be what country will be selected. > linked view
-    var clickedDataset = dataset[0];
+    var clickedDataset = [dataset[0]];
 
     // draw map
-    var linkedMap = new Datamap({element: document.getElementById('map_place')});
+    var linkedMap = new Datamap({
+                  element: document.getElementById('map_place'),
+                    fills: {
+                      defaultFill: 'grey'
+                    },
+                    data: 
 
 
+                  });
+
+
+    var clickedDataset = [dataset[0]];
     // // scatter dataset
     // var regionData = {
     //   "Americas": [],
@@ -100,20 +109,22 @@ window.onload = function() {
 
 
 
-function drawBar(dataset, chartPad, limit){
+function drawBar(dataset, chartPad, limit) {
 
   // create the svg field
   var svg = d3v5.select("body")
             .append("svg")
               .attr("width", chartPad.w)
-              .attr("height", chartPad.h / 2);
+              .attr("height", chartPad.h);
+
 
   var ageLimit = 100;
+
 
   // create x, y scaling for placing data in svg pixels
   var xScale = getXscale(ageLimit, chartPad, dataset);
 
-  var xScale2 = getXscale2(xScale);
+  // var xScale2 = getXscale2(xScale);
 
   var yScale = getYscale(ageLimit, chartPad);
 
@@ -124,90 +135,200 @@ function drawBar(dataset, chartPad, limit){
   var yAxis = d3v5.axisLeft(yScale);
 
 
-  // define interaction tooltip
-  var avBarTip = d3v5.tip()
-                     .attr('class', 'd3-tip')
-                     .offset([-10, 0])
-                     .html(function(d) {
-                       console.log(d);
-                       return d["Country"] + " HPI Rank: " + d["HPI Rank"] + "<br>"
-                              + "Average Life Expectancy: " + d["Average_Life_Expectancy"]
-                     });
+  // // define interaction tooltip
+  // var avBarTip = d3v5.tip()
+  //                    .attr('class', 'd3-tip')
+  //                    .offset([-10, 0])
+  //                    .html(function(d) {
+  //                      return d["Country"] + " HPI Rank: " + d["HPI Rank"] + "<br>"
+  //                             + "Average Life Expectancy: " + d["Average_Life_Expectancy"];
+  //
+  //                    });
+  //
+  //
+  // var adjBarTip = d3v5.tip()
+  //                     .attr('class', 'd3-tip')
+  //                     .offset([-10, 0])
+  //                     .html(function(d) {
+  //                       return d["Country"] + " HPI Rank: " + d["HPI Rank"] + "<br>"
+  //
+  //
+  //                     });
 
-  var adjBarTip = d3v5.tip()
-                      .attr('class', 'd3-tip')
-                      .offset([-10, 0])
-                      .html(function(d) {
-                        console.log(d);
-                        return d["Country"] + " HPI Rank: " + d["HPI Rank"] + "<br>"
-                               + "Inequality-adjusted Life Expectancy: " + d["Inequality-adjusted_Life_Expectancy"]
-                      });
-
-
-  // make interactive animation work
-  svg.call(avBarTip);
-
-  svg.call(adjBarTip);
-
-
-  // draw the grouped bars
-  var groupBar = svg.selectAll("groupBar")
-                      .data(dataset)
-                      .enter()
-                    .append("groupBar")
-                      .attr("class", "chart")
-                      .attr("transform", function(d) {
-                        return 'translate(${xScale(d.Country)}, 0)'
-                      });
+  var tip = d3v5.tip()
+                .attr('class', 'd3-tip')
+                .offset([-10, 0])
+                .html(function(d) {
+                  return d[0]["Country"] + " HPI Rank: " + d[0]["HPI_Rank"] + "<br>"
+                         + "Average Life Expectancy: " + Math.round(+d[0]["Average_Life_Expectancy"], 4)
+                         + "<br>" + "Inequality-adjusted Life Expectancy: "
+                         + Math.round(+d[0]["Inequality-adjusted_Life_Expectancy"], 4);
+                })
+                .attr("stroke", "black");;
 
 
-  // add bars to groupchart
-  groupBar.selectAll("avLifeExp")
-          .data(function(d) {
-            return [d];
-          })
-          .enter()
-        .append("avLifeExp")
-          .attr("class", "avLifeExp")
-          .style("fill", "#f9eb25")
-          .attr("x", function(d) {
-            return "Average_Life_Expectancy"
-          })
-          .attr("y", function(d) {
-            return yScale(d["Average_Life_Expectancy"])
-          })
-          .attr("width", xScale2.bandwidth())
-          .attr("height", function(d) {
-            return (chartPad.h - yScale(+d["Average_Life_Expectancy"]) - chartPad.top)
-          })
-          .on('mouseover', avBarTip.show) // for animation
-          .on('mouseout', avBarTip.hide);
+  // // make interactive animation work
+  // svg.call(avBarTip);
+  //
+  // svg.call(adjBarTip);
+
+  svg.call(tip);
+
+
+  // create the bars
+  var rect = svg.selectAll("rect")
+                .data([dataset, dataset])
+                .enter()
+                .append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d, i) {
+
+                  let string = i === 0 ? "Average_Life_Expectancy" : "Inequality-adjusted_Life_Expectancy";
+
+                  return xScale(string);
+
+                 })
+                .attr("y", function(d, i) {
+
+                  let string = i === 0 ? "Average_Life_Expectancy" : "Inequality-adjusted_Life_Expectancy";
+
+                  return yScale(d[0][string]); // can use + for converting string to int
+
+                 })
+                .attr("width", xScale.bandwidth())
+                .attr("height", function(d, i) {
+                  let string = i === 0 ? "Average_Life_Expectancy" : "Inequality-adjusted_Life_Expectancy";
+
+                  return  (chartPad.h - yScale(+d[0][string]) - chartPad.top);
+
+                 })
+                .attr("fill", function(d, i) {
+
+                  let string = i === 0 ? "Average_Life_Expectancy" : "Inequality-adjusted_Life_Expectancy";
+                  if (string == "Average_Life_Expectancy") {
+                    return "red"
+                  }
+                  else {
+                    return "yellow"
+                  };
+
+                })
+                .on('mouseover', tip.show)
+                .on('mouseout', tip.hide);
+
+
+  // create x axis by calling on xAxis
+  svg.append("g")
+     .attr("class", "axis")
+     .attr("transform", "translate(0," + (chartPad.h - chartPad.top) + ")")
+     .call(xAxis);
+
+
+  // create yAxis by calling on yAxis
+  svg.append("g")
+     .attr("class", "axis")
+     .attr("transform", "translate(" + chartPad.top + ",0)")
+     .call(yAxis);
+
+
+  // create chart Title
+  svg.append("text")
+     .attr("transform", "translate(100,0)")
+     .attr("x", 50)
+     .attr("y", 50)
+     .attr("font-size", "24px")
+     .text("Life expectancy");
+
+
+  country = dataset[0].Country;
+
+
+  // create x-axis label
+  svg.append("text")
+     .attr("class", "x label")
+     .attr("text-anchor", "end")
+     .attr("x", chartPad.w / 2)
+     .attr("y", chartPad.h - 50)
+     .text(country);
+
+
+  // create y-axis label
+  svg.append("text")
+     .attr("class", "y label")
+     .attr("text-anchor", "end")
+     .attr("y", 50)
+     .attr("x", -150)
+     .attr("dy", "1em")
+     .attr("transform", "rotate(-90)")
+     .text("Age (in years)");
 
 
 
-  groupbar.selectAll("adjLifeExp")
-          .data(function(d) {
-            console.log(d)
-            return [d];
-          })
-          .enter()
-        .append("adjLifeExp")
-          .attr("class", "adjLifeExp")
-          .style("fill", "#f91109")
-          .attr("x", function(d) {
-            return "Inequality-adjusted_Life_Expectancy"
-          })
-          .attr("y", function(d) {
-            return yScale(d["Inequality-adjusted_Life_Expectancy"])
-          })
-          .attr("width", xScale2.bandwidth())
-          .attr("height", function(d) {
-            return (chartPad.h - yScale(+d["Inequality-adjusted_Life_Expectancy"]) - chartPad.top)
-          })
-          .on('mouseover', adjBarTip.show) // for animation
-          .on('mouseout', adjBarTip.hide);
 
-                // .attr("x", function(d) {
+
+
+
+
+
+  //
+  // // draw the grouped bars
+  // var groupBar = svg.selectAll("groupBar")
+  //                     .data(dataset)
+  //                     .enter()
+  //                   .append("groupBar")
+  //                     .attr("class", "chart")
+  //                     .attr("transform", function(d) {
+  //                       return 'translate(${xScale(d.Country)}, 0)'
+  //                     });
+  //
+  //
+  // // add bars to groupchart
+  // groupBar.selectAll("avLifeExp")
+  //         .data(function(d) {
+  //           return d;
+  //         })
+  //         .enter()
+  //       .append("avLifeExp")
+  //         .attr("class", "avLifeExp")
+  //         .style("fill", "#f9eb25")
+  //         .attr("x", function(d) {
+  //           return "Average_Life_Expectancy"
+  //         })
+  //         .attr("y", function(d) {
+  //           return yScale(d["Average_Life_Expectancy"])
+  //         })
+  //         .attr("width", xScale2.bandwidth())
+  //         .attr("height", function(d) {
+  //           return (chartPad.h - yScale(+d["Average_Life_Expectancy"]) - chartPad.top)
+  //         })
+  //         .on('mouseover', avBarTip.show) // for animation
+  //         .on('mouseout', avBarTip.hide);
+  //
+  //
+  //
+  // groupbar.selectAll("adjLifeExp")
+  //         .data(function(d) {
+  //           console.log(d)
+  //           return [d];
+  //         })
+  //         .enter()
+  //       .append("adjLifeExp")
+  //         .attr("class", "adjLifeExp")
+  //         .style("fill", "#f91109")
+  //         .attr("x", function(d) {
+  //           return "Inequality-adjusted_Life_Expectancy"
+  //         })
+  //         .attr("y", function(d) {
+  //           return yScale(d["Inequality-adjusted_Life_Expectancy"])
+  //         })
+  //         .attr("width", xScale2.bandwidth())
+  //         .attr("height", function(d) {
+  //           return (chartPad.h - yScale(+d["Inequality-adjusted_Life_Expectancy"]) - chartPad.top)
+  //         })
+  //         .on('mouseover', adjBarTip.show) // for animation
+  //         .on('mouseout', adjBarTip.hide);
+  //
+  //               // .attr("x", function(d) {
                 //    return xScale(d["HPI Rank"]);
                 //
                 // })
@@ -230,29 +351,25 @@ function drawBar(dataset, chartPad, limit){
 
 
 function getXscale(limit, chartPad, dataset) {
-  var xScale = d3v5.scaleBand()
-                   .domain(dataset.Country)
-                   .range([chartPad.left, chartPad.w - chartPad.right])
-                   .padding(chartPad.betweenBar);
+
+  var xScale = d3v5.scaleLinear()
+                   .domain([0, 1])
+                   .range([0, chartPad.w - chartPad.left - 10]);
+
+  xScale = d3v5.scaleBand()
+              .domain(["Average_Life_Expectancy", "Inequality-adjusted_Life_Expectancy"])
+              .range([100, 400]);
 
   return xScale;
 
 };
 
 
-function getXscale2(xScale) {
-  var xScale2 = d3v5.scaleBand()
-                    .domain(["Average_Life_Expectancy", "Inequality-adjusted_Life_Expectancy"])
-                    .range([0, xScale.bandwidth()])
-  console.log("xScale2 is being defined")
-  console.log(xScale2)
-};
-
-
 function getYscale(limit, chartPad) {
+
   var yScale = d3v5.scaleLinear()
-                   .domain([0, limit])
-                   .range([chartPad.h - chartPad.bottom, chartPad.top]);
+                 .domain([0, limit])
+                 .range([chartPad.h - chartPad.bottom, chartPad.top]);
 
   return yScale;
 
